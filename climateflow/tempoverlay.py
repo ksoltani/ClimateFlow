574157c61c63b5ebe197f9bebbde00dde38be999
Nick <n.gibbons@uq.edu.au>
2016-07-31 18:46:03 +1000

    readded pickle files

--- climateflow/tempoverlay.py
+++ climateflow/tempoverlay.py
@@ -16,6 +16,7 @@ import bokeh.plotting as bp
 from bokeh.models.tiles import WMTSTileSource
 from datashader.bokeh_ext import InteractiveImage
 from collateData import loadDicts, plotFormat, getChosenData
+from pylab import plot, show
 
 
 sDir, sYDir = loadDicts(2)
@@ -25,6 +26,11 @@ RE = 6.317e6 # Earth Radius
 
 mN = np.radians(np.array(lat))*RE
 mW = np.radians(np.array(lon))*RE
+temp = np.array([float('NaN') if i==-322.0 else i for i in temp])
+temp = (temp-temp.min())
+temp = temp/temp.max()
+
+
 
 def base_plot(prange, tools='pan,wheel_zoom,reset', webgl=False):
     p = bp.figure(tools=tools,
@@ -44,16 +50,18 @@ def image_callback(x_range, y_range, w, h):
     cvs = ds.Canvas(
           plot_width=w,plot_height=h,x_range=x_range,y_range=y_range)
     agg = cvs.points(df,'meterswest','metersnorth', ds.mean('temp'))
-    img = tf.interpolate(agg, cmap = cmap, how='eq_hist')
-    return tf.dynspread(img,threshold=0.75, max_px=8)
+    img = tf.interpolate(agg, cmap = cmap, how='cbrt',span=[0.0,1.0])
+    return tf.dynspread(img,threshold=0.5, max_px=4)
 
 
 print("Begin...")
 #df = pd.read_hdf('census.h5', 'census') # Too big
 
 background = "black"
-USA = ((-13884029,  -7453304), (2698291, 6455972))
-BNE = ((-3054524.63492, 17015047.67515038), (-3055524.63492, 17016047.67515038))
+USA = ((-13884029,       -7453304), (2698291, 6455972))
+BNE = (( -3054524.63492, 17015047.67515038),
+       ( -3055524.63492, 17016047.67515038))
+
 plot_width  = int(1000)
 plot_height = int(plot_width*7.0/12)
 
@@ -64,12 +72,12 @@ df = pd.DataFrame(
       })
 print(df.tail())
 
-display(HTML("<style>.container { width:100% !important; }</style>"))
+#display(HTML("<style>.container { width:100% !important; }</style>"))
 
 bp.output_file('overlay.html',autosave=True)
 p = base_plot(BNE)
 
-cmap = colormap_select(Hot,0.2, reverse=(background!="black"))
+cmap = colormap_select(Hot, reverse=(background!="black"))
 url="http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{Z}/{Y}/{X}.png"
 #url="http://tile.stamen.com/toner-background/{Z}/{X}/{Y}.png"
 tile_renderer = p.add_tile(WMTSTileSource(url=url))
