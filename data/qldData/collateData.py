ca29b96b521cfe52cd8d59c72d94e36896bf92a3
Nick <n.gibbons@uq.edu.au>
2016-07-31 13:36:39 +1000

    Modularised the data collater

--- data/qldData/collateData.py
+++ data/qldData/collateData.py
@@ -1,77 +1,96 @@
+""" 
+Collate a stack of .zip files into python
+
+@author: Kyle Damm and Kianoosh Soltani
+"""
+
 import os
 import shutil
 from math import log
 import zipfile
 import os.path
 
-stationsFile = open('alphaQLD_36.txt','r')
-lines = stationsFile.readlines()
-stationCode = []
-print(lines[2])
-codeColStart = lines[2].find('Site')
-nameColStart = lines[2].find('Name')
-latColStart= lines[2].find('Lat')
-lonColStart = lines[2].find('Lon')
-startColStart = lines[2].find('Start')
-endColStart = lines[2].find('End')
-yearsColStart = lines[2].find('Years')
-percentColStart = lines[2].find('%')
-awsColStart = lines[2].find('AWS')
+def makeDirectories(stationFilename):
+    """ Return stationsDirectory and stationYearDirectory """
+    with open(stationFilename, 'r') as stationsFile:
+        lines = stationsFile.readlines()
+    stationCode = []
+    print(lines[2])
+    codeColStart = lines[2].find('Site')
+    nameColStart = lines[2].find('Name')
+    latColStart= lines[2].find('Lat')
+    lonColStart = lines[2].find('Lon')
+    startColStart = lines[2].find('Start')
+    endColStart = lines[2].find('End')
+    yearsColStart = lines[2].find('Years')
+    percentColStart = lines[2].find('%')
+    awsColStart = lines[2].find('AWS')
+    
+    dataLines = lines[4:]
+    stationsDirectory = {}
+    # Go through the file and produce a list of all stations
+    for l in dataLines:
+        code = l[0:nameColStart].strip()
+        if len(code) < 6:
+            code = (6 - len(code)) * '0'+ code
+        name = l[nameColStart:latColStart].strip()
+        lat = l[latColStart:lonColStart].strip()
+        lon = l[lonColStart:startColStart].strip()
+        start= l[startColStart:endColStart].strip()
+        end = l[endColStart:yearsColStart].strip()
+        years = l[yearsColStart:percentColStart].strip()
+        percent = l[percentColStart:awsColStart].strip()
+        aws = l[awsColStart:].strip()
+        #data = [code, name, lat, lon, start, end, years, percent, aws]
+        stationsDirectory[code] = [name, lat, lon]
+    stationYearDirectory = {}
+    noDataStations = []
+    # Find the stations that don't have data
+    for s in stationsDirectory.keys():
+        if not os.path.isfile(s+str('.zip')):
+            noDataStations.append(s)
+    
+    # remove directories that don't have any data
+    for c in noDataStations:
+        stationsDirectory.pop(c)
+    # Look through the directories
+    for s in stationsDirectory.keys():
+    
+        monthArray = ['Jan', 'Feb', 'Mar', 'April', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        zip = zipfile.ZipFile(s+str('.zip'))
+        zip.extractall()
+        os.remove('IDCJAC0002_'+s+'_Note.txt')
+        os.remove('IDCJAC0002_'+s+'_Data1.csv')
+        filename = 'IDCJAC0002_'+s+'_Data12.csv'
+        with open(filename, 'r') as f:
+            lines = f.readlines()
+            lines = lines[1:]
+            count = 0
+            for l in lines:
+                cleanLine = l.strip().split(',') 
+                data = cleanLine[3 : 15] # monthly data
+                #data = list(map(makefloat(data)))
+                year = cleanLine[2]
+                # record the start year
+                if count == 0:
+                    startYear = year    
+                #print(s)
+                stationYearDirectory[(s, year)] = data # Year is the second index
+                count = count + 1
+            endYear = year # record the last year for station s
+            # add startYear and endYear data to the station directory info
+            stationsDirectory[s].append(startYear)
+            stationsDirectory[s].append(endYear)
 
-dataLines = lines[4:]
-stationsDirectory = {}
-# Go through the file and produce a list of all stations
-for l in dataLines:
-    code = l[0:nameColStart].strip()
-    if len(code) < 6:
-        code = (6 - len(code)) * '0'+ code
-    name = l[nameColStart:latColStart].strip()
-    lat = l[latColStart:lonColStart].strip()
-    lon = l[lonColStart:startColStart].strip()
-    start= l[startColStart:endColStart].strip()
-    end = l[endColStart:yearsColStart].strip()
-    years = l[yearsColStart:percentColStart].strip()
-    percent = l[percentColStart:awsColStart].strip()
-    aws = l[awsColStart:].strip()
-    #data = [code, name, lat, lon, start, end, years, percent, aws]
-    stationsDirectory[code] = [name, lat, lon]
-stationYearDirectory = {}
-noDataStations = []
-# Find the stations that don't have data
-for s in stationsDirectory.keys():
-    if not os.path.isfile(s+str('.zip')):
-        noDataStations.append(s)
+    return stationsDirectory, stationYearDirectory
 
-# remove directories that don't have any data
-for c in noDataStations:
-    stationsDirectory.pop(c)
-# Look through the directories
-for s in stationsDirectory.keys():
-
-    monthArray = ['Jan', 'Feb', 'Mar', 'April', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    zip = zipfile.ZipFile(s+str('.zip'))
-    zip.extractall()
-    os.remove('IDCJAC0002_'+s+'_Note.txt')
-    os.remove('IDCJAC0002_'+s+'_Data1.csv')
-    filename = 'IDCJAC0002_'+s+'_Data12.csv'
-    with open(filename, 'r') as f:
-        lines = f.readlines()
-        lines = lines[1:]
-        count = 0
-        for l in lines:
-            cleanLine = l.strip().split(',') 
-            data = cleanLine[3 : 15] # monthly data
-            year = cleanLine[2]
-            # record the start year
-            if count == 0:
-                startYear = year    
-            #print(s)
-            stationYearDirectory[(s, year)] = data # Year is the second index
-            count = count + 1
-        endYear = year # record the last year for station s
-        # add startYear and endYear data to the station directory info
-        stationsDirectory[s].append(startYear)
-        stationsDirectory[s].append(endYear)
+def makefloat(string):
+    """ Tolerant float conversion """
+    try:
+        num = float(string)
+    except(ValueError):
+        num = -322.0
+    return num
 
 def getChosenData(month, year ,stationYearDirectory, stationsDirectory):
     """ month : int
@@ -106,5 +125,23 @@ def getChosenData(month, year ,stationYearDirectory, stationsDirectory):
         
     return result
 
-for i in range(1900, 2016, 1):        
-    print getChosenData(1, i, stationYearDirectory, stationsDirectory)
+def plotFormat(data):
+    """ Take the output of getChosenData and return 
+    three arrays corresponding to plottable lat, long, maxtemp
+    """
+    lat, lon, temp = list(zip(*data))
+    lat = list(map(makefloat, lat))
+    lon = list(map(makefloat, lon))
+    temp = list(map(makefloat, temp))
+    return lat, lon, temp
+
+
+if __name__ == '__main__':
+    sDir, sYDir = makeDirectories('alphaQLD_36.txt')
+    for i in range(1900, 1920, 1):        
+        data= getChosenData(1, i, sYDir, sDir)
+        print(data[0])
+
+    data = getChosenData(8, 1992, sYDir, sDir)
+    lat, lon, temp = plotFormat(data)
+    print(lat[0:5], lon[0:5], temp[0:5])
